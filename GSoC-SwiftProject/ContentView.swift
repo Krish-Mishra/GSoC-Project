350e7159d05820cea62aa48465380d1f21fc972d
Krish Mishra <rm25101980@gmail.com>
2026-02-26 01:29:43 +0530

    Initial Commit

---
+++ GSoC-SwiftProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GSoC-SwiftProject
+//
+//  Created by Krish Mishra on 26/02/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GSoC-SwiftProject/GSoC_SwiftProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  GSoC_SwiftProjectApp.swift
+//  GSoC-SwiftProject
+//
+//  Created by Krish Mishra on 26/02/26.
+//
+
+import SwiftUI
+
+@main
+struct GSoC_SwiftProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
